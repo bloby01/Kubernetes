b252b0ded51b50409c382cd9b9f76b08565d6392
blob <57810141+bloby01@users.noreply.github.com>
2020-04-16 13:15:21 +0200

    Update deploiement-k8s-V-0.2.3.sh

--- deploiement-k8s-V-0.2.3.sh
+++ deploiement-k8s-V-0.2.3.sh
@@ -512,7 +512,7 @@ key DDNS_UPDATE {
   secret "bad" ;
 };
 EOF
-secret=`grep Key: /root/*.private | cut -f 2 -d " "` && \
+secret=`grep Key: ./*.private | cut -f 2 -d " "` && \
 sed -i -e "s|bad|$secret|g" /etc/named/ddns.key && \
 chown root:named /etc/named/ddns.key && \
 chmod 640 /etc/named/ddns.key && \
