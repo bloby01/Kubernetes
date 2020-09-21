8ff2f429bee565a661bb792a9cce99d9ba9b0a7d
blob <57810141+bloby01@users.noreply.github.com>
2020-09-21 23:39:43 +0200

    Update deploiement-k8s_CentOS8-V-1.0.0.sh

--- deploiement-k8s_CentOS8-V-1.0.0.sh
+++ deploiement-k8s_CentOS8-V-1.0.0.sh
@@ -224,7 +224,7 @@ vrai="1"
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el8-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
