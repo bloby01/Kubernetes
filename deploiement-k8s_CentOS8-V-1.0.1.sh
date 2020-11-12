376164f702e26b13ca6e8edda877b811f1ead3de
blob <57810141+bloby01@users.noreply.github.com>
2020-11-12 09:17:27 +0100

    Update deploiement-k8s_CentOS8-V-1.0.1.sh

--- deploiement-k8s_CentOS8-V-1.0.1.sh
+++ deploiement-k8s_CentOS8-V-1.0.1.sh
@@ -109,7 +109,7 @@ vrai="1"
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum-config-manager --enable docker-ce-stable
-yum -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+#yum -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 yum  install  -y containerd.io && \
 #yum install -y wget
 #wget -O dockerinstall.sh https://get.docker.com
