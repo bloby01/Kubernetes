77fd11ffe320611412bf5c18e2d06d2bd6542da6
blob <57810141+bloby01@users.noreply.github.com>
2021-01-21 11:35:16 +0100

    Update deploiement_GitLab-v1.sh

--- deploiement_GitLab-v1.sh
+++ deploiement_GitLab-v1.sh
@@ -6,10 +6,10 @@ curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script
 dnf install -y gitlab-ce
 dnf install -y epel-release
 dnf install -y certbot
-certbot certonly --rsa-key-size 4096 --standalone --agree-tos --no-eff-email --email root@gitlab.mon.dom -d gitlab.mon.dom
+certbot certonly --rsa-key-size 4096 --standalone --agree-tos --no-eff-email --email christophe@cmconsulting.online -d gitlab.mon.dom
 openssl dhparam -out /etc/gitlab/dhparams.pem 4096
 chmod 600 /etc/gitlab/dhparams.pem
-sed -i 's/external_url 'http://gitlab.example.com'/external_url 'https://gitlab.mon.dom'/g' /etc/gitlab/gitlab.rb
+sed -i 's|external_url 'http://gitlab.example.com'|external_url 'https://gitlab.mon.dom'|g' /etc/gitlab/gitlab.rb
 echo "nginx['redirect_http_to_https'] = true" >> /etc/gitlab/gitlab.rb
 echo "nginx['ssl_certificate'] = "/etc/letsencrypt/live/gitlab.mon.dom/fullchain.pem" " >> /etc/gitlab/gitlab.rb
 echo "nginx['ssl_certificate_key'] = "/etc/letsencrypt/live/gitlab.mon.dom/privkey.pem" " >> /etc/gitlab/gitlab.rb
