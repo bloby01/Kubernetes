e10bfa5b1c7994215689392b0de73a32447b8b10
blob <57810141+bloby01@users.noreply.github.com>
2021-10-31 19:40:59 +0100

    Update deploiement_multi_master-V-1.0.sh

--- deploiement_multi_master-V-1.0.sh
+++ deploiement_multi_master-V-1.0.sh
@@ -1071,7 +1071,7 @@ verif
 #
 #
 vrai="1"
-kubeadm join "172.21.0.100:6443" --token ${token}  --discovery-token-ca-cert-hash sha256:${tokenca} && \
+kubeadm join "master1-k8s.mon.dom:6443" --token ${token}  --discovery-token-ca-cert-hash sha256:${tokenca} && \
 vrai="0"
 nom="Etape ${numetape} - Intégration du noeud worker au cluster"
 verif
