4afe9ff923577d82753b4d8ddfe3c91f9ad8c406
blob <57810141+bloby01@users.noreply.github.com>
2022-12-27 23:10:38 +0100

    Update deploiement_multi_master-V-3.0.sh

--- deploiement_multi_master-V-3.0.sh
+++ deploiement_multi_master-V-3.0.sh
@@ -983,7 +983,7 @@ ssh root@loadBalancer-k8s.mon.dom systemctl restart haproxy.service
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "      Déploiement Kubernetes en cours "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-source < ~/.bashrc
+source /root/.bashrc
 kubeadm init --control-plane-endpoint="`host loadBalancer-k8s.mon.dom | cut -f 4 -d " "`:6443" --upload-certs  --pod-network-cidr="192.168.0.0/16" &> /root/noeudsupplementaires.txt && \
 #################################################
 # 
@@ -1079,7 +1079,7 @@ fi
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "      Déploiement d'un nouveau master en cours "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-source < ~/.bashrc
+source /root/.bashrc
 kubeadm join 172.21.0.100:6443 --token ${token} --discovery-token-ca-cert-hash sha256:${tokensha} ${CertsKey}  && \
 vrai="0"
 nom="Etape ${numetape} - Intégration du noeud  au cluster K8S"
@@ -1211,7 +1211,7 @@ vrai="1"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "      Déploiement d'un nouveau worker en cours "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-source < ~/.bashrc
+source /root/.bashrc
 kubeadm join "172.21.0.100:6443" --token ${token}  --discovery-token-ca-cert-hash sha256:${tokencaworker} && \
 vrai="0"
 nom="Etape ${numetape} - Intégration du noeud worker au cluster"
