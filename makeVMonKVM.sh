fa25025a4bbc7cba3df7c2a7b899413c93cfa7a8
blob <57810141+bloby01@users.noreply.github.com>
2025-03-21 13:26:18 +0100

    Update makeVMonKVM.sh

--- makeVMonKVM.sh
+++ makeVMonKVM.sh
@@ -1,5 +1,81 @@
 #!/bin/sh
+#########################################################################
+#									#
+# Outil de construction de VMs dans un environnement Linux KVM By	#
+#			ste.cmc.merle@gmail.com				#
+#									#
+# - Vérification de la présence des outils et installation		#
+# - Création d'un network sans dhcp					#
+# - Création de disque							#
+# - Téléchargement ISO RockyLinux					#					
+# - Création de VM et boot sur ISO					#
+#########################################################################
+#
+#	Configuration des app de virtualisation sur le system hôte
+#
+systemHote(){
+dnf install -y qemu-kvm libvirt virt-install bridge-utils dnsmasq && \
+systemctl enable --now libvirtd
+cat <<EOF | tee network-k8s.xml
+<network>
+  <name>nat-k8s</name>
+  <bridge name="virbr1"/>
+  <forward mode="nat"/>
+  <ip address="172.21.0.1" netmask="255.255.255.0">
+  </ip>
+</network>
+EOF
+virsh net-define network-k8s.xml
+virsh net-start nat-k8s
+virsh net-autostart nat-k8s
+}
+#
+#################################################
+#
+#	Creation des disques pour les VMs
+#
+createDiskVms(){
+echo -n "quelle doit être la taille du disque principal de la VM [ex: 20] ? : "
+read tailleDisquePrincipal
+qemu-img create -f qcow2 ${noeud}${x}.qcow2 ${tailleDisquePrincipal}G
+}
+#
+#################################################
+#
+#	Download de l'iso
+#
+download(){
+if [ -f rocky.iso ]
+then
+echo "le fichier iso de rocky est disponible : $(ls -lh rocky.iso)"
+sleep 4
+else
+wget -O rocky.iso https://dl.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9-latest-x86_64-boot.iso
+isoRocky=rocky.iso
+fi
+}
+#
+#################################################
+#
+#	Construction des VMs
+#
+constructionVM(){
+virt-install --name VM-${noeud}${x}.qcow2 --ram 3072 --vcpus 2 --disk path=${noeud}${x}.qcow2,format=qcow2 --cdrom ${isoRocky} --boot cdrom --os-type linux --os-variant rocky9 --network network=nat-k8s,model=virtio --graphics vnc --virt-type kvm --hvm
+}
+#################################################
+# 
+#Fonction de vérification des étapes
+#
 
+verif(){
+	numetape=`expr ${numetape} + 1 `
+	  if [ "${vrai}" -eq "0" ]; then
+	    echo "Machine: ${node}${x}-k8s.mon.dom - ${nom} - OK"
+	  else
+	    echo "Erreur sur Machine: ${node}${x}-k8s.mon.dom - ${nom} - OUPSSS "
+	    exit 0
+	  fi
+}
 ###################################################################################################
 #                                                                                                 #
 #                             Debut de la séquence d'Installation                                 #
