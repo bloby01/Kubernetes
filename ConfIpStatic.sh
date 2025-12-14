c409cc082dd1dc628a1057c1f829dbbb996ff6c5
blob <57810141+bloby01@users.noreply.github.com>
2025-12-14 15:14:51 +0100

    Add static IP configurations for multiple devices

--- ConfIpStatic.sh
+++ ConfIpStatic.sh
@@ -11,22 +11,33 @@ read choix
 case $choix in
     A)
         # configuration IP master1
-        
+        ip a a dev enp0s3 172.21.0.101/24
+        ip route add 0.0.0.0/0 via  172.21.0.100
         ;;
     B)
         # configuration IP master2
+        ip a a dev enp0s3 172.21.0.102/24
+        ip route add 0.0.0.0/0 via  172.21.0.100
         ;;
     C)
         # configuration IP master3
+        ip a a dev enp0s3 172.21.0.103/24
+        ip route add 0.0.0.0/0 via  172.21.0.100
         ;;
     1)
         # configuration IP worker1
+        ip a a dev enp0s3 172.21.0.104/24
+        ip route add 0.0.0.0/0 via  172.21.0.100
         ;;
     2)
         # configuration IP worker2
+        ip a a dev enp0s3 172.21.0.105/24
+        ip route add 0.0.0.0/0 via  172.21.0.100
         ;;
     3)
         # configuration IP worker3
+        ip a a dev enp0s3 172.21.0.106/24
+        ip route add 0.0.0.0/0 via  172.21.0.100
         ;;
     *)
         # Relancer le script
